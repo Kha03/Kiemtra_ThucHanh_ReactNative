11bc9b26a9ae6f7359d3796fb11b7a0bb769740e
Kha <minhkha20031996@gmail.com>
2024-11-10 10:52:49 +0700

    done

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import WelCome from "./components/WelCome";
+import BikeList from "./components/BikeList";
+import BikeDetail from "./components/BikeDetail";
+import AddProductScreen from "./components/AddProductScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Provider } from "react-redux";
+import store from "./store/store";
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={WelCome} />
+          <Stack.Screen name="BikeList" component={BikeList} />
+          <Stack.Screen name="AddProductScreen" component={AddProductScreen} />
+          <Stack.Screen name="BikeDetail" component={BikeDetail} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
